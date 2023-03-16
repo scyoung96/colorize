3938733a346f3a2d73185643d91f56bdca281b7e
Spencer Young <scyoung96@gmail.com>
2023-03-16 02:13:54 -0600

    refactor

--- __init__.py
+++ __init__.py
@@ -1,38 +1,90 @@
+colors = {'blue': '027', 'cyan': '045', 'green': '048', 'lime': '154', 'yellow': '011', 'orange': '202', 'red': '124', 'pink': '213', 'purple': '129', 'violet': '055'}
+
 def colorize(string, *args):
-    colors = {'blue': '027', 'cyan': '045', 'green': '048', 'lime': '154', 'yellow': '011', 'orange': '202', 'red': '124', 'pink': '213', 'purple': '129', 'violet': '055'}
-    string = str(string)
-    str_len = len(string)
-    split_len = int(str_len / len(args))
+    '''Colorize a string. Arguments must be of type str or tuple. If tuple, the tuple must be of type (str, int), where str is a color and int is number of characters.'''
+    
+    typeCheck = checkArgs(args)
 
-    if len(args) == 1:
-        return f'\033[38;5;{colors[args[0]]}m{string}\033[0;0m'
+    if typeCheck == str:
+        if len(args) == 1:
+            return f'\033[38;5;{colors[args[0]]}m{string}\033[0;0m'
 
-    else:
-        ret_str = ''
+        else:
+            strLen = len(string)
+            splitLen = int(strLen / len(args))
+            retStr = ''
+
+            for i in args:
+                retStr += f'\033[38;5;{colors[i]}m{string[:splitLen]}\033[0;0m'
+                string = string[splitLen:]
+            
+            if splitLen * len(args) != strLen:
+                retStr += f'\033[38;5;{colors[args[-1]]}m{string}\033[0;0m'
+
+            return retStr
+
+    elif typeCheck == tuple:
+        retStr = ''
 
         for i in args:
-            ret_str += f'\033[38;5;{colors[i]}m{string[:split_len]}\033[0;0m'
-            string = string[split_len:]
+            retStr += f'\033[38;5;{colors[i[0]]}m{string[:i[1]]}\033[0;0m'
+            string = string[i[1]:]
 
-        if split_len * len(args) != str_len:
-            ret_str += f'\033[38;5;{colors[i]}m{string}\033[0;0m'
+        return retStr
 
-    return ret_str
 
 def printc(string, *args):
-    if len(args) == 1:
-        print(colorize(string, args[0]))
+    '''Print a string with color. Arguments must be of type str or tuple. If tuple, the tuple must be of type (str, int), where str is a color and int is number of characters.'''
+   
+    typeCheck = checkArgs(args)
 
-    else:
-        str_len = len(string)
-        split_len = int(str_len / len(args))
-        ret_str = ''
+    if typeCheck == str:
+        if len(args) == 1:
+            print(f'\033[38;5;{colors[args[0]]}m{string}\033[0;0m')
+
+        else:
+            strLen = len(string)
+            splitLen = int(strLen / len(args))
+            retStr = ''
+
+            for i in args:
+                retStr += f'\033[38;5;{colors[i]}m{string[:splitLen]}\033[0;0m'
+                string = string[splitLen:]
+            
+            if splitLen * len(args) != strLen:
+                retStr += f'\033[38;5;{colors[args[-1]]}m{string}\033[0;0m'
+
+            print(retStr)
+
+    elif typeCheck == tuple:
+        retStr = ''
 
         for i in args:
-            ret_str += colorize(string[:split_len], i)
-            string = string[split_len:]
-        
-        if split_len * len(args) != str_len:
-            ret_str += colorize(string, i)
+            retStr += f'\033[38;5;{colors[i[0]]}m{string[:i[1]]}\033[0;0m'
+            string = string[i[1]:]
 
-        print(ret_str)
+        print(retStr)
+
+
+def checkArgs(args):
+    _type = None
+
+    for i in args:
+        if _type == None:
+            _type = type(i)
+        elif _type != type(i):
+            raise TypeError("Arguments must be of the same type")
+        
+        if type(i) == tuple:
+            if len(i) != 2:
+                raise ValueError("Tuple arguments must have a length of 2")
+            elif type(i[0]) != str or type(i[1]) != int:
+                raise TypeError("Tuple arguments must be of type (str, int)")
+        
+        elif type(i) != str:
+            raise TypeError("Arguments must be of type str or tuple")
+        
+        else:
+            pass
+
+    return _type
